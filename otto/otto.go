fd9fa7568e8fc0ca9d476ead7fe8bdfd7e0c20db
Rusty Eddy <rusty@rustyeddy.com>
2018-07-28 18:01:03 -0700

    Timeservice works

--- otto/otto.go
+++ otto/otto.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"flag"
-	"net"
-	"io"
 	log "github.com/rustyeddy/logrus" 
 )
 
+// Context has all the variables we need to satisfy a request
+type Context struct {
+	Configuration
+}
+
+// Configuration items are variables that can be set in a configuration
+// file at startup time (or read in during run time)
 type Configuration struct {
 	Hostport string // "<hostname>:<port>"
-	Proto string
+	Proto string // tcp, udp, http ..
+	TimeServer bool
 }
 
 var (
@@ -21,51 +26,14 @@ var (
 func init() {
 	flag.StringVar(&cfg.Hostport, "h", "localhost:1231", "set the server host")
 	flag.StringVar(&cfg.Proto, "P", "tcp", "TCP, HDP or ...")
+	flag.BoolVar(&cfg.TimeServer, "time", true, "start the time server port 1231 by default ")
+
+	log.Debug("Init complete")
 }
 
 func main() {
 	fmt.Println("OttO ... ")
-	ln, err := net.Listen("tcp", "localhost:1231")
-	if err != nil {
-		log.Fatalf("tcp localhost:1231 err ", err)
-	}
-
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatalf("listen %s %s %v", cfg.Proto, cfg.Hostport)
-			continue
-		}
-		go handleConn(conn)
-	}
+	StartTimeService()
 }
 
-func handleConn(c net.Conn) {
-	defer c.Close()
-	for {
-		t := time.Now()
-		_, err := io.WriteString(c, t.Format(time.RFC3339) + "\n")
-		if err != nil {
-			return 
-		}
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func spinner(delay time.Duration) {
-	for {
-		for _, r := range `-\|/` {
-			fmt.Printf("\r%c", r)
-			time.Sleep(delay)
-		}
-	}
-}
-
-// fib
-func fib(x int) int {
-	if x < 2 {
-		return x
-	}
-	return fib(x-1) + fib(x-2)
-}
 
